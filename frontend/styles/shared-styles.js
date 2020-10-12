8ca396fd35c64b83de3b5add1efb3a1527dc4eb2
Jakub Popiół <popioljakub@gmail.com>
2020-10-12 19:05:51 +0200

    vaadin template start

---
+++ frontend/styles/shared-styles.js
@@ -0,0 +1,52 @@
+// eagerly import theme styles so as we can override them
+import '@vaadin/vaadin-lumo-styles/all-imports';
+
+const $_documentContainer = document.createElement('template');
+
+$_documentContainer.innerHTML = `
+<custom-style>
+  <style>
+    html {
+      --lumo-primary-color: hsl(120, 66%, 28%);
+    }
+
+    [theme~="dark"] {
+      --lumo-primary-color: hsl(140, 0%, 0%);
+      --lumo-primary-text-color: hsl(214, 0%, 99%);
+      --lumo-base-color: hsl(142, 100%, 13%);
+    }
+  </style>
+</custom-style>
+
+
+<custom-style>
+  <style>
+    html {
+      overflow:hidden;
+    }
+  </style>
+</custom-style>
+
+<dom-module id="app-layout" theme-for="vaadin-app-layout">
+  <template>
+    <style>
+      :host(:not([dir='rtl']):not([overlay])) [part='drawer'] {
+        border-right: none;
+        box-shadow: var(--lumo-box-shadow-s);
+        background-color: var(--lumo-base-color);
+        z-index: 1;
+      }
+      :host([dir='rtl']:not([overlay])) [part='drawer'] {
+        border-left: none;
+        box-shadow: var(--lumo-box-shadow-s);
+        background-color: var(--lumo-base-color);
+        z-index: 1;
+      }
+      [part='navbar'] {
+        box-shadow: var(--lumo-box-shadow-s);
+      }
+    </style>
+  </template>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer.content);
---
+++ src/main/java/com/jakpop/stepsdictionary/views/about/AboutView.java
@@ -0,0 +1,20 @@
+package com.jakpop.stepsdictionary.views.about;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.jakpop.stepsdictionary.views.main.MainView;
+
+@Route(value = "about", layout = MainView.class)
+@PageTitle("About")
+@CssImport("./styles/views/about/about-view.css")
+public class AboutView extends Div {
+
+    public AboutView() {
+        setId("about-view");
+        add(new Label("Content placeholder"));
+    }
+
+}
