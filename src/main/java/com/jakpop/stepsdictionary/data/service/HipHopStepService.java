fb9a0e2f3a3c504eed3165e843ca0f2c563827b3
Jakub Popiół <popioljakub@gmail.com>
2020-12-09 17:11:01 +0100

    fixed hip hop section

--- src/main/java/com/jakpop/stepsdictionary/data/service/HipHopStepService.java
+++ src/main/java/com/jakpop/stepsdictionary/data/service/HipHopStepService.java
@@ -3,6 +3,7 @@ package com.jakpop.stepsdictionary.data.service;
 import com.jakpop.stepsdictionary.data.entity.HipHopStep;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,17 +20,14 @@ public class HipHopStepService extends CrudService<HipHopStep, String> {
         return repository;
     }
 
-    public List<HipHopStep> findByParams(String name, String creator, String period) {
+    public List<HipHopStep> findByParams(String name, String period) {
         List<HipHopStep> steps = repository.findAll();
 
-        if (name != null) {
-            steps = steps.stream().filter(user -> user.getName().equals(name)).collect(Collectors.toList());
+        if (StringUtils.isNotBlank(name)) {
+            steps = steps.stream().filter(step -> step.getName().equals(name)).collect(Collectors.toList());
         }
-        if (creator != null) {
-            steps = steps.stream().filter(user -> user.getCreator().equals(creator)).collect(Collectors.toList());
-        }
-        if (period != null) {
-            steps = steps.stream().filter(user -> user.getPeriod().equals(period)).collect(Collectors.toList());
+        if (StringUtils.isNotBlank(period)) {
+            steps = steps.stream().filter(step -> step.getPeriod().equals(period)).collect(Collectors.toList());
         }
 
         return steps;
