e5a70555b10d49fa9163012394b05be42b5db75b
Jakub Popiół <popioljakub@gmail.com>
2020-12-12 11:25:23 +0100

    cleanup

--- src/main/java/com/jakpop/stepsdictionary/data/generator/DataGenerator.java
+++ src/main/java/com/jakpop/stepsdictionary/data/generator/DataGenerator.java
@@ -1,30 +1,22 @@
 package com.jakpop.stepsdictionary.data.generator;
 
-import com.jakpop.stepsdictionary.data.entity.enums.Role;
-import com.jakpop.stepsdictionary.data.entity.users.User;
 import com.jakpop.stepsdictionary.data.service.UserRepository;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 
 
-
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
 @SpringComponent
+@Slf4j
 public class DataGenerator {
 
     @Bean
     public CommandLineRunner loadData(UserRepository userRepository) {
         return args -> {
-            Logger logger = LoggerFactory.getLogger(getClass());
 
-//            userRepository.save(new User("user", "u", Role.USER));
-//            userRepository.save(new User("admin", "a", Role.ADMIN));
-
-            logger.info("Generated demo data");
+            log.info("Generated demo data");
         };
     }
 
--- src/main/java/com/jakpop/stepsdictionary/views/login/RegisterView.java
+++ src/main/java/com/jakpop/stepsdictionary/views/login/RegisterView.java
@@ -7,7 +7,6 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
